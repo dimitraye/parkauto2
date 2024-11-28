67e42331709725bbe08431065819da2bc9a2941b
Dimitri MOMPEROUSSE <momperousse.dimitri.pro@gmail.com>
2024-11-28 14:32:20 +0100

    first commit

---
+++ src/main/java/com/example/parkauto2/parkauto2/Parkauto2Application.java
@@ -0,0 +1,13 @@
+package com.example.parkauto2.parkauto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parkauto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parkauto2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/parkauto2/parkauto2/Parkauto2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkauto2.parkauto2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parkauto2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
